5cb693e02a5096553914751fb3bec74510e7fb2b
ledisalvo <ledisalvo@gmail.com>
2020-07-28 18:07:29 -0300

    Section 3 - 18. Build our first component

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
 
-function App() {
+export default function App() {
+  console.log('EventDashboard: ', EventDashboard);
   return (
     <div className="App">
-      <h1>Re-vents</h1>      
+      <h1>Re-vents</h1>     
+      <EventDashboard />       
     </div>
   );
 }
-
-export default App;
