db1e3add8e156dfeac245ca031b809793f2f7d7c
ledisalvo <ledisalvo@gmail.com>
2020-07-30 16:03:31 -0300

    Section 3 - 24. React component state

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar';
 import { Container } from 'semantic-ui-react';
 
 export default function App() {
-  console.log('EventDashboard: ', EventDashboard);
+  const [formOpen, setFormOpen] = useState(false);
   return (
     <>
-      <NavBar />
+      <NavBar setFormOpen={setFormOpen} />
       <Container className='main'>
-        <EventDashboard />       
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />       
       </Container>
     </>
   );
