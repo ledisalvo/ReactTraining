017776ca382e42e1bc2c6472129f59526d43f28c
ledisalvo <ledisalvo@gmail.com>
2020-08-01 18:03:00 -0300

    Section 5 - 38. Using navlinks and links

--- revents/src/features/nav/NavBar.jsx
+++ revents/src/features/nav/NavBar.jsx
@@ -1,25 +1,26 @@
-    import React from 'react';
-    import { Menu, Container, Button } from 'semantic-ui-react';
+import React from 'react';
+import { Menu, Container, Button } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
-    export default function NavBar({createEvent}) {
-        return (
-            <Menu inverted fixed='top'>
-                <Container>
-                    <Menu.Item header >
-                        <img src="/assets/logo.png" alt="logo" style={{marginRight: 15}}/>
-                        Re-vents
-                    </Menu.Item>
-                    <Menu.Item name="Events" />
-                    <Menu.Item>
-                        <Button onClick={() => createEvent()} positive inverted content="Create Event"/>
-                    </Menu.Item>
-                    <Menu.Item position="right">
-                        <Button basic inverted content="Login"/>
-                        <Button basic inverted content="Register" style={{marginLeft: '0.5em'}}/>
-                    </Menu.Item>
-                    <Menu name="Events" />
-                </Container>
+export default function NavBar({createEvent}) {
+    return (
+        <Menu inverted fixed='top'>
+            <Container>
+                <Menu.Item header as={NavLink} exact to='/'>
+                    <img src="/assets/logo.png" alt="logo" style={{marginRight: 15}}/>
+                    Re-vents
+                </Menu.Item>
+                <Menu.Item name="Events" as={NavLink} to='/events' />
+                <Menu.Item as={NavLink} to='/createEvent'>
+                    <Button positive inverted content="Create Event"/>
+                </Menu.Item>
+                <Menu.Item position="right">
+                    <Button basic inverted content="Login"/>
+                    <Button basic inverted content="Register" style={{marginLeft: '0.5em'}}/>
+                </Menu.Item>
+                <Menu name="Events" />
+            </Container>
 
-            </Menu>
-        );
-    }
+        </Menu>
+    );
+}
